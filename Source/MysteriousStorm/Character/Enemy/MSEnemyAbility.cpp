bed3fee2b976be4de7163722d526651cde9a72e5
zjulzy <lenmainlzy@gmail.com>
2024-09-24 13:55:53 +0800

    add melee enemy

--- Source/MysteriousStorm/Character/Enemy/MSEnemyAbility.cpp
+++ Source/MysteriousStorm/Character/Enemy/MSEnemyAbility.cpp
@@ -3,3 +3,20 @@
 
 #include "MSEnemyAbility.h"
 
+#include "MysteriousStorm/Character/MSCharacter.h"
+
+bool UMSEnemyAbility::TryActivateAbility_Implementation()
+{
+	if (Super::TryActivateAbility_Implementation())
+	{
+		Cast<AMSCharacter>(GetWorld()->GetFirstPlayerController()->GetPawn())->GetAttributeComponent()->Hurt(OwnerEnemy, Damage);
+		return true;
+	}
+	return false;
+}
+
+bool UMSEnemyAbility::CheckPrecondition_Implementation()
+{
+	return Super::CheckPrecondition_Implementation() && (FVector::Distance(
+		GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation(), OwnerEnemy->GetActorLocation()) < 100);
+}
